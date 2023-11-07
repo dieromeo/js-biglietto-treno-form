a9e4485f6d3addad9b8d12836704267cf4d7c88e
dieromeo <diegoromeo99@icloud.com>
2023-11-07 18:59:20 +0100

    feat: style

--- JS/main.js
+++ JS/main.js
@@ -7,31 +7,39 @@ Utilizando il codice javascript precedente, e apportando migliorie ove necessari
 
 const generateButton = document.getElementById('generate') // bottone per generare il biglietto
 const annullaButton = document.getElementById('Annulla') // bottone per annullare il biglietto
-var sectionBiglietto = document.getElementById('dr-biglietto'); // sezione che deve comparire al click
+let sectionBiglietto = document.getElementById('dr-biglietto'); // sezione che deve comparire al click
+
 
 generateButton.addEventListener('click', function () {
     const km = document.getElementById('km-number').value; // numero di km
+
     const nomeUtente = document.getElementById('nome-utente').value; // aggiunta nome utente
     document.getElementById('passeggero-output').innerHTML = nomeUtente;
+
     const carrozza = Math.floor(Math.random() * 14 + 1); // aggiunta numero carrozza
     document.getElementById('carrozza').innerHTML = carrozza;
+
     const codiceCp = Math.floor(Math.random() * 100000); // aggiunta codice biglietto
     document.getElementById('codice-cp').innerHTML = codiceCp;
-    var prezzoBiglietto = km * 0.21;
+
+    let prezzoBiglietto = km * 0.21;
     if (document.getElementById('eta').value === "Minorenne") { //caso minorenne
         prezzoBiglietto = prezzoBiglietto * 0.8;
-        console.log('Il tuo biglietto scontato per minorenni costa: ', prezzoBiglietto.toFixed(2));
+        console.log('Il tuo biglietto scontato per minorenni costa: ', prezzoBiglietto);
+        document.getElementById('offerta').innerHTML = "Offerta minorenni"
     }
 
     else if (document.getElementById('eta').value === "Over 65") { // caso over 65
         prezzoBiglietto = prezzoBiglietto * 0.6;
-        console.log('Il tuo biglietto scontato per over 65 costa: ', prezzoBiglietto.toFixed(2));
+        console.log('Il tuo biglietto scontato per over 65 costa: ', prezzoBiglietto);
+        document.getElementById('offerta').innerHTML = "Offerta over"
     }
     else { // caso base
-        console.log('Il tuo biglietto costa: ', prezzoBiglietto.toFixed(2));
+        console.log('Il tuo biglietto costa: ', prezzoBiglietto);
+        document.getElementById('offerta').innerHTML = "Offerta standard"
     }
 
-    document.getElementById('prezzo').innerHTML = prezzoBiglietto;
+    document.getElementById('prezzo').innerHTML = prezzoBiglietto.toFixed(2) + "€";
 
     sectionBiglietto.classList.remove('d-none');
 })
