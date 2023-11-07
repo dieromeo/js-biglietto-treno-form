4b04b6a2067b677f4339f2682c40cbf7c0f5b879
dieromeo <diegoromeo99@icloud.com>
2023-11-07 18:19:25 +0100

    feat: section biglietto

--- JS/main.js
+++ JS/main.js
@@ -7,12 +7,16 @@ Utilizando il codice javascript precedente, e apportando migliorie ove necessari
 
 const generateButton = document.getElementById('generate') // bottone per generare il biglietto
 const annullaButton = document.getElementById('Annulla') // bottone per annullare il biglietto
-var sectionBiglietto = document.getElementById('dr-biglietto');
+var sectionBiglietto = document.getElementById('dr-biglietto'); // sezione che deve comparire al click
 
 generateButton.addEventListener('click', function () {
     const km = document.getElementById('km-number').value; // numero di km
-    const nomeUtente = document.getElementById('nome-utente').value;
+    const nomeUtente = document.getElementById('nome-utente').value; // aggiunta nome utente
     document.getElementById('passeggero-output').innerHTML = nomeUtente;
+    const carrozza = Math.floor(Math.random() * 14 + 1); // aggiunta numero carrozza
+    document.getElementById('carrozza').innerHTML = carrozza;
+    const codiceCp = Math.floor(Math.random() * 100000); // aggiunta codice biglietto
+    document.getElementById('codice-cp').innerHTML = codiceCp;
     var prezzoBiglietto = km * 0.21;
     if (document.getElementById('eta').value === "Minorenne") { //caso minorenne
         prezzoBiglietto = prezzoBiglietto * 0.8;
@@ -26,6 +30,8 @@ generateButton.addEventListener('click', function () {
     else { // caso base
         console.log('Il tuo biglietto costa: ', prezzoBiglietto.toFixed(2));
     }
+
+    document.getElementById('prezzo').innerHTML = prezzoBiglietto;
 
     sectionBiglietto.classList.remove('d-none');
 })
